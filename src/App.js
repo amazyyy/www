62a63f85422675373e21aaf51cb9e38c6e5b493c
Andy Chen <amazingandyyy@gmail.com>
2020-07-27 00:16:28 -0700

    chore: update styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <p>yyy</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
