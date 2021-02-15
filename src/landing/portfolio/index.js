d4261cb80da6dbe89f0c4a88d7a38fdec8b2ca11
Andy Chen <amazingandyyy@gmail.com>
2021-02-14 21:35:17 -0800

    feat: add colorful background

--- src/landing/portfolio/index.js
+++ src/landing/portfolio/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 import './style.scss'
 import Hodldex from './hodldex'
 import Mapitout from './mapitout'
+import Yours from './yours'
 
 export default () => (<div className='portfolio-component container'>
   <div className='landing-big-title'>Projects</div>
   <Hodldex />
   <Mapitout />
+  <Yours />
 </div>)
